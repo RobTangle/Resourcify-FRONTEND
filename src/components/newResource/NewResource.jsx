e9cd363de1bcdb7b9a9d09a76691a8d22b40d1a4
eub1 <ebonforte@gmail.com>
2023-01-12 22:42:46 -0300

    newResource modal version1 without styles

--- src/components/newResource/NewResource.jsx
+++ src/components/newResource/NewResource.jsx
@@ -1,8 +1,42 @@
+import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createResource } from "../../redux/features/resource";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-40%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "grey",
+  },
+};
+
+// Modal.setAppElement("#yourAppElement");
 
 export function NewResource({ isLoggedIn }) {
+  //! REACT MODAL :
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  //! ---------------------------
+
   const dispatch = useDispatch();
 
   const [form, setForm] = useState({
@@ -34,9 +68,27 @@ export function NewResource({ isLoggedIn }) {
     <>
       {isLoggedIn === true && (
         <div>
-          <hr />
-          <h2>Save new resource</h2>
-          <fieldset>
+           {/* // <!-- Modal toggle --> */}
+      <button
+        onClick={openModal}
+        data-modal-target="authentication-modal"
+        data-modal-toggle="authentication-modal"
+        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+      >
+        New Resource
+      </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+         <h2>Save new resource</h2>
+         <hr />
+         <br/>
+         <fieldset>
             <form action="" onSubmit={handleSubmit} className="form">
               <label htmlFor="title">Title</label>
               <input type="text" id="title" onChange={handleOnChange} />
@@ -54,9 +106,12 @@ export function NewResource({ isLoggedIn }) {
               <input type="number" />
               <label htmlFor="keywords">Keywords</label>
               <input type="text" id="keywords" onChange={handleOnChange} />
+              <br/>
               <button>Save</button>
             </form>
           </fieldset>
+           {/* <button onClick={closeModal}>Close X</button> */}
+          </Modal>
         </div>
       )}
     </>
