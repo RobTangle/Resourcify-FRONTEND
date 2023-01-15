2d1d679e21f477d1bdbf8af5ea9fbacbb81c795c
Robert0101 <rob.tangled@gmail.com>
2023-01-15 10:43:54 -0300

    Cambiar detalles de createResource fn

--- src/redux/features/resource/resourceThunk.js
+++ src/redux/features/resource/resourceThunk.js
@@ -1,44 +1,24 @@
 import Swal from "sweetalert2";
 import axios from "axios";
-import { NAME_ACCESS_TOKEN, header } from "../../../helpers/constants";
+import { header } from "../../../helpers/constants";
 
 import { URL_CREATE_NEW_RESOURCE } from "../../../helpers/URLs";
 import { setUserProfile } from "../user/userSlice";
 
-// export function fetchAllResources() {
-//   return async function (dispatch) {
-//     try {
-//       const accessToken = localStorage.getItem(NAME_ACCESS_TOKEN);
-//       let response = await axios.get(
-//         URL_SERVER + "/bookmarks",
-//         header(accessToken)
-//       );
-//       if (response.status === 200) {
-//         return dispatch(setAllResources(response.data));
-//       }
-//     } catch (error) {
-//       Swal.fire({
-//         title: "Oops! Something went wrong",
-//         text: error?.response?.data?.message?.[0] || error.message,
-//         icon: "error",
-//         showConfirmButton: true,
-//         showCloseButton: true,
-//       });
-//     }
-//   };
-// }
-
-export function createResource(form, setForm, token) {
+export function createResource(form, setForm, accessToken) {
+  console.log("createResource 1");
   return async function (dispatch) {
     try {
-      const accessToken = localStorage.getItem(NAME_ACCESS_TOKEN) || token;
+      console.log("createResource 2 adentro de try catch");
+      // const accessToken = localStorage.getItem(NAME_ACCESS_TOKEN) || token;
       const response = await axios.post(
         URL_CREATE_NEW_RESOURCE,
         form,
         header(accessToken)
       );
       console.log("reponse = ", response);
-      if (response.status === 201 || response.status === 200) {
+      if (response.status === 201) {
+        console.log("Response status === 201");
         Swal.fire({
           position: "center",
           icon: "success",
@@ -49,16 +29,18 @@ export function createResource(form, setForm, token) {
         });
         setForm({
           title: "",
-          description: "",
           link: "",
           category: "",
+          description: "",
+          order: 0,
+          is_favourite: false,
           keywords: "",
         });
       }
       // La request me responde con el usuario actualizado:
       return dispatch(setUserProfile(response.data));
     } catch (error) {
-      console.log(error);
+      console.log("error en createNewResource! ", error);
       return Swal.fire({
         position: "center",
         icon: "error",
