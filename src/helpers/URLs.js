19cb20d7a631266547795993b3c0c466db9d8979
eub1 <ebonforte@gmail.com>
2023-01-15 23:37:47 -0300

    update url_exists_in_db to match main

--- src/helpers/URLs.js
+++ src/helpers/URLs.js
@@ -1,5 +1,5 @@
 export const URL = "http://localhost:3333";
 export const URL_SERVER = "http://localhost:3333";
-export const URL_EXISTS_IN_DB = "http://localhost:3333/users/exists";
+export const URL_EXISTS_IN_DB = "http://localhost:3333/user/exists";
 export const URL_CREATE_NEW_RESOURCE = URL + "/source";
 export const URL_GET_USER_INFO = URL + "/users/userInfo";
