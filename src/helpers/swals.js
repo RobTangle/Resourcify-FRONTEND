b587e7db385eff747d7ff61a4882a0afb054af98
Robert0101 <rob.tangled@gmail.com>
2023-01-16 11:37:16 -0300

    Crear mixins

--- src/helpers/swals.js
+++ src/helpers/swals.js
@@ -1,5 +1,5 @@
 import Swal from "sweetalert2";
-import { URL_SERVER } from "./URLs";
+import { URL } from "./URLs";
 import axios from "axios";
 
 export const signInFormMX = (setToken, setIsLoggedIn) =>
@@ -16,10 +16,7 @@ export const signInFormMX = (setToken, setIsLoggedIn) =>
       };
       try {
         console.log("USER INPUTS = ", userInputs);
-        const response = await axios.post(
-          URL_SERVER + "/auth/signin",
-          userInputs
-        );
+        const response = await axios.post(URL + "/auth/signin", userInputs);
         if (response.status === 200) {
           localStorage.setItem(
             "access_token_bookmarks",
@@ -62,10 +59,7 @@ export const signUpFormMX = Swal.mixin({
     };
     console.log("USER INPUTS = ", userInputs);
     try {
-      const response = await axios.post(
-        URL_SERVER + "/auth/signup",
-        userInputs
-      );
+      const response = await axios.post(URL + "/auth/signup", userInputs);
       if (response.status === 201) {
         Swal.fire({
           title: "You've been registered!",
@@ -82,3 +76,50 @@ export const signUpFormMX = Swal.mixin({
     }
   },
 });
+
+export const SwalErrorMX = (error) =>
+  Swal.mixin({
+    title: "Oops! Something went wrong! 😬",
+    text: error?.response?.data?.message?.[0] || error.message,
+    icon: "error",
+  });
+
+export const SwalSuccessMX = (titleText) =>
+  Swal.mixin({
+    title: titleText,
+    icon: "success",
+    position: "center",
+    showConfirmButton: true,
+    timer: 3000,
+    timerProgressBar: true,
+  });
+
+export const ToastSuccessMX = (titleText) =>
+  Swal.mixin({
+    toast: true,
+    title: titleText,
+    position: "top-end",
+    icon: "success",
+    showConfirmButton: false,
+    timer: 2500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+export const ToastErrorMX = (error) =>
+  Swal.mixin({
+    toast: true,
+    title: error?.response?.data?.message?.[0] || error.message,
+    position: "top-end",
+    icon: "error",
+    showConfirmButton: false,
+    timer: 2500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
