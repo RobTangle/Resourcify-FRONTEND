93dc221f65884a421d7c13326c4e8eeaa586b44b
Robert0101 <rob.tangled@gmail.com>
2023-01-16 19:23:17 -0300

    Agregar styles para que el link no se overfluya

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,6 +3,7 @@ import { ModalEditResource } from "../ModalEditResource/ModalEditResource";
 import { deleteResource } from "../../redux/features/resource";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useState } from "react";
+import "./card.style.css";
 
 export function Card({ resource }) {
   const dispatch = useDispatch();
@@ -76,22 +77,24 @@ export function Card({ resource }) {
           </span>
         ) : null}
       </div>
-      <a
-        href={resource?.link}
-        target="_blank"
-        className="inline-flex items-center text-blue-600 hover:underline"
-      >
-        {resource?.link}
-        <svg
-          className="mt-3 mb-3 w-5 h-5 ml-2"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
+      <div>
+        <a
+          href={resource?.link}
+          target="_blank"
+          className="inline-flex items-center text-blue-600 hover:underline custom-class"
         >
-          <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
-          <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
-        </svg>
-      </a>
+          {resource?.link}
+          <svg
+            className="mt-3 mb-3 w-5 h-5 ml-2"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
+            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
+          </svg>
+        </a>
+      </div>
       <div>
         {Array.isArray(resource.keywords) && resource.keywords?.length
           ? resource.keywords.map((keyword) => {
