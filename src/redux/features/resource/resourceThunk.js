4f555456e99b97a824682cc7cd6a28953a8d3ff5
Robert0101 <rob.tangled@gmail.com>
2023-01-16 11:37:52 -0300

    Modificar funciones para adaptarlas a nuevos mixins

--- src/redux/features/resource/resourceThunk.js
+++ src/redux/features/resource/resourceThunk.js
@@ -1,7 +1,5 @@
-import Swal from "sweetalert2";
 import axios from "axios";
 import { header } from "../../../helpers/constants";
-
 import {
   URL_CREATE_NEW_RESOURCE,
   URL_S_PA_UPDATE_RESOURCE,
@@ -10,13 +8,15 @@ import {
 import { setUserProfile } from "../user/userSlice";
 import { filterResources } from "../../../helpers/filterResources";
 import { setRenderized } from "./resourceSlice";
+import {
+  SwalErrorMX,
+  ToastErrorMX,
+  ToastSuccessMX,
+} from "../../../helpers/swals";
 
 export function createResource(form, setForm, accessToken) {
-  console.log("createResource 1");
   return async function (dispatch) {
     try {
-      console.log("createResource 2 adentro de try catch");
-      // const accessToken = localStorage.getItem(NAME_ACCESS_TOKEN) || token;
       const response = await axios.post(
         URL_CREATE_NEW_RESOURCE,
         form,
@@ -24,15 +24,7 @@ export function createResource(form, setForm, accessToken) {
       );
       console.log("reponse = ", response);
       if (response.status === 201) {
-        console.log("Response status === 201");
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Resource created!",
-          showConfirmButton: true,
-          timer: 5000,
-          timerProgressBar: true,
-        });
+        ToastSuccessMX("Resource created!").fire();
         setForm({
           title: "",
           link: "",
@@ -47,102 +39,53 @@ export function createResource(form, setForm, accessToken) {
       return dispatch(setUserProfile(response.data));
     } catch (error) {
       console.log("error en createNewResource! ", error);
-      return Swal.fire({
-        position: "center",
-        icon: "error",
-        title: "Oops! Something went wrong!",
-        text: error?.response?.data?.message?.[0] || error.message,
-        showConfirmButton: true,
-      });
+      ToastErrorMX(error).fire();
     }
   };
 }
 
-export function editResource(id, form, setForm, accessToken) {
-  console.log("EDIT RESOURCE");
+export function editResource(id, form, accessToken) {
   return async function (dispatch) {
     try {
-      console.log("EDIT RESOURCE adentro de try catch");
-      // const accessToken = localStorage.getItem(NAME_ACCESS_TOKEN) || token;
       const response = await axios.patch(
         URL_S_PA_UPDATE_RESOURCE + `/${id}`,
         form,
         header(accessToken)
       );
-      console.log("reponse = ", response);
       if (response.status === 200) {
-        console.log("Response status === 200");
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Resource updated!",
-          showConfirmButton: true,
-          timer: 5000,
-          timerProgressBar: true,
-        });
-        setForm({
-          title: "",
-          link: "",
-          category: "",
-          description: "",
-          order: 0,
-          is_favourite: false,
-          keywords: "",
-        });
+        ToastSuccessMX("Resource edited!").fire();
       }
       // La request me responde con el usuario actualizado:
       return dispatch(setUserProfile(response.data));
     } catch (error) {
       console.log("error en createNewResource! ", error);
-      return Swal.fire({
-        position: "center",
-        icon: "error",
-        title: "Oops! Something went wrong!",
-        text: error?.response?.data?.message?.[0] || error.message,
-        showConfirmButton: true,
-      });
+      ToastErrorMX(error).fire();
     }
   };
 }
 
 export function deleteResource(id, accessToken, renderizedArray) {
-  console.log("DELETE RESOURCE");
-  console.log("ACCESS TOKEN EN DELETE RESOURCE = ", accessToken);
   return async function (dispatch) {
     try {
-      console.log("DELETE RESOURCE adentro de try catch");
-      // const accessToken = localStorage.getItem(NAME_ACCESS_TOKEN) || token;
       const response = await axios.delete(
         URL_S_DE_DELETE_RESOURCE + `/${id}`,
         header(accessToken)
       );
       console.log("reponse = ", response);
-      if (response.status === 204) {
-        console.log("Response status === 204");
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Resource deleted!",
-          showConfirmButton: true,
-          timer: 5000,
-          timerProgressBar: true,
-        });
+      if (response.status === 200) {
+        ToastSuccessMX("Resource deleted!").fire();
+        // La request me responde con el usuario actualizado:
+        dispatch(setUserProfile(response.data));
+        const renderizedArrayUpdated = renderizedArray.filter(
+          (doc) => doc._id !== id
+        );
+        return dispatch(setRenderized(renderizedArrayUpdated));
+      } else {
+        throw new Error("Conflictive response status.");
       }
-      // La request me responde con el usuario actualizado:
-      dispatch(setUserProfile(response.data));
-      const renderizedArrayUpdated = renderizedArray.filter(
-        (doc) => doc._id !== id
-      );
-      return dispatch(setRenderized(renderizedArrayUpdated));
     } catch (error) {
-      console.log("error en createNewResource! ", error);
-      return Swal.fire({
-        position: "center",
-        icon: "error",
-        title: "Oops! Something went wrong!",
-        text: error?.response?.data?.message?.[0] || error.message,
-        showConfirmButton: true,
-      });
+      console.log(error);
+      SwalErrorMX(error).fire();
     }
   };
 }
@@ -157,11 +100,7 @@ export function filterElements(filterObj, userAllResources) {
       );
       dispatch(setRenderized(filteredElements));
     } catch (error) {
-      Swal.error({
-        title: "Oop! Something went wrong! 😬",
-        text: error.message,
-        icon: "error",
-      });
+      SwalErrorMX(error).fire();
     }
   };
 }
@@ -175,11 +114,7 @@ export function resetRenderized(userAllResources) {
       );
       dispatch(setRenderized(userAllResources));
     } catch (error) {
-      Swal.fire({
-        title: "Oop! Something went wrong! 😬",
-        text: error.message,
-        icon: "error",
-      });
+      SwalErrorMX(error).fire();
     }
   };
 }
