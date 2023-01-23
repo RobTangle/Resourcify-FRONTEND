d5926725617669d155645052b4982dfff41d2162
Robert0101 <rob.tangled@gmail.com>
2023-01-23 10:48:50 -0300

    Crear const URL para parsear link

--- src/helpers/URLs.js
+++ src/helpers/URLs.js
@@ -8,3 +8,4 @@ export const URL_EXISTS_IN_DB = URL + "/user/exists";
 export const URL_CREATE_NEW_RESOURCE = URL + "/source";
 export const URL_S_PA_UPDATE_RESOURCE = URL + "/source";
 export const URL_S_DE_DELETE_RESOURCE = URL + "/source";
+export const URL_S_G_PARSE_LINK = URL + "/source/link/?link=";
