b83abe228871ebfe128756c11e8a058e263a1863
Robert0101 <rob.tangled@gmail.com>
2023-01-25 11:27:28 -0300

    fix/Fix auto title + mover keywords input más arriba

--- src/components/Forms/FormEdit.jsx
+++ src/components/Forms/FormEdit.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 import { editResource } from "../../redux/features/resource";
+import axios from "axios";
+import { header } from "../../helpers/constants";
+import { URL_S_G_PARSE_LINK } from "../../helpers/URLs";
 
 export function FormEdit({ resource }) {
   const [form, setForm] = useState({
@@ -25,6 +28,26 @@ export function FormEdit({ resource }) {
 
   function handleOnChange(e) {
     setForm({ ...form, [e.target.id]: e.target.value });
+  }
+
+  // Esta función hace un Get al servidor para obtener el title de la url, y valoriza el form.title según la respuesta del Get:
+  async function handleOnChangeLink(e) {
+    try {
+      setForm({ ...form, link: e.target.value });
+      const accessToken = await getAccessTokenSilently();
+      const response = await axios.get(
+        URL_S_G_PARSE_LINK + e.target.value,
+        header(accessToken)
+      );
+      // const html = await response.text();
+      const html = response.data.html;
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      const title = doc.querySelector("title").innerText;
+      setForm({ ...form, title, link: e.target.value });
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
   async function handleOnSubmit(e) {
@@ -51,7 +74,7 @@ export function FormEdit({ resource }) {
           id="link"
           value={form.link}
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          onChange={handleOnChange}
+          onChange={handleOnChangeLink}
           required
         />
       </div>
@@ -89,6 +112,21 @@ export function FormEdit({ resource }) {
       </div>
       <div className="mb-4">
         <label
+          for="keywords"
+          className="block mb-0 text-sm font-medium text-gray-900 dark:text-black"
+        >
+          Keywords
+        </label>
+        <input
+          type="text"
+          id="keywords"
+          value={form.keywords}
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          onChange={handleOnChange}
+        />
+      </div>
+      <div className="mb-4">
+        <label
           for="description"
           className="block mb-0 text-sm font-medium text-gray-900 dark:text-black"
         >
@@ -120,21 +158,7 @@ export function FormEdit({ resource }) {
           onChange={handleOnChange}
         />
       </div>
-      <div className="mb-4">
-        <label
-          for="keywords"
-          className="block mb-0 text-sm font-medium text-gray-900 dark:text-black"
-        >
-          Keywords
-        </label>
-        <input
-          type="text"
-          id="keywords"
-          value={form.keywords}
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          onChange={handleOnChange}
-        />
-      </div>
+
       <div className="flex items-start mb-6">
         <div className="flex items-center h-5">
           <input
