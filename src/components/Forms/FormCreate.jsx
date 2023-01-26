3f13dde2bfa3736ae74a70104b033854dd846743
Robert0101 <rob.tangled@gmail.com>
2023-01-25 23:35:09 -0300

    Add maxLength to title, category and description

--- src/components/Forms/FormCreate.jsx
+++ src/components/Forms/FormCreate.jsx
@@ -88,6 +88,7 @@ export function FormCreate() {
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
           onChange={handleOnChange}
           value={form.title}
+          maxLength={120}
           required
         />
       </div>
@@ -104,6 +105,7 @@ export function FormCreate() {
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
           onChange={handleOnChange}
           value={form.category}
+          maxLength={35}
           required
         />
       </div>
@@ -134,6 +136,7 @@ export function FormCreate() {
           id="description"
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
           onChange={handleOnChange}
+          maxLength={900}
           value={form.description}
         ></textarea>
       </div>
